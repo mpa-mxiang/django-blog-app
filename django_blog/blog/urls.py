d7187924d8a79ac57eb709922b87d3b10ff3b304
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-27 23:57:11 -0400

    fix signup and login links issue

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("login/", views.user_login),
-    path("signup/", views.signup)
+    path("", views.signup),
+    path("login/", views.user_login)
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'blog/index.html')
 
 def user_login(request):
     return render(request, 'blog/login.html')
