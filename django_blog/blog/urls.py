71b4f340a9d18affc33a18574ed6acd0b7a6ba97
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-27 23:44:52 -0400

    create urls and views for login and signup

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("login/", views.user_login),
+    path("signup/", views.signup)
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def user_login(request):
+    return render(request, 'blog/login.html')
+
+def signup(request):
+    return render(request, 'blog/signup.html')
