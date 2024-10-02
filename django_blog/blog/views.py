b696b4b28409fc77c9f5fe43c4fda0f245d1dcf4
Jiachun Xiang <jcxiang1230@gmail.com>
2024-10-01 23:38:53 -0400

    complete signup view with save a new user and redirect

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Posts
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,6 +9,13 @@ def user_login(request):
     return render(request, 'blog/login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        email = request.POST.get('uemail')
+        password = request.POST.get('upassword')
+        new_user = User.objects.create_user(username = name, email = email, password = password)
+        new_user.save()
+        return redirect('/login')
     return render(request, 'blog/signup.html')
     
 def home(request):
