c9649d27a4eb52de45d196d3f529f7e08534ee85
Jiachun Xiang <jcxiang1230@gmail.com>
2024-10-01 23:56:08 -0400

    complete login view with authenticate

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Posts
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def user_login(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        password = request.POST.get('upassword')
+        login_user = authenticate(request, username = name, password = password)
+        if login_user is not None:
+            login(request, login_user)
+            return redirect('/')
+        else:
+            return redirect('/login')
     return render(request, 'blog/login.html')
 
 def signup(request):
